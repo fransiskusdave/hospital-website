c0882f2457ecc5600818eb6c2dc9bbbd788fbf73
Fransiskus Dave <davefransiskus@gmail.com>
2024-01-23 21:17:29 +0700

    added some css variables and classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: "15px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1330px",
+    },
+    extend: {
+      colors: {
+        primary: "#242a2b",
+        secondary: "#808080",
+        accent: {
+          DEFAULT: "#1cbccf",
+          secondary: "#18abbc",
+          tertiary: "#90c6cd",
+        },
+        grey: "#e8f0f1",
+      },
+      fontFamily: {
+        primary: "Poppins",
+      },
+      boxShadow: {
+        custom1: " 0px 2px 40px 0px rgba(8, 70, 78, 0.08)",
+        custom2: " 0px 0px 30px 0px rgba(8, 73, 81, 0.06)",
+      },
+      backgroundImage: {
+        services: "url(./assets/img/services/bg.svg)",
+        testimonials: "url(./assets/img/testimonials/bg.svg)",
+        departments: "url(./assets/img/departments/bg.svg)",
+        quoteLeft: "url(./assets/icons/testimonials/quote-left.svg)",
+        quoteRight: "url(./assets/icons/testimonials/quote-right.svg)",
+      },
+    },
   },
   plugins: [],
 };
