420023912cf63ba6f15fb3e7851669548273154a
Fransiskus Dave <davefransiskus@gmail.com>
2024-02-01 22:00:52 +0700

    added show answer event on faq

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,17 @@ const swiper = new Swiper(".swiper", {
     clickable: true,
   },
 });
+
+// FAQ Show Answer
+const faqItems = document.querySelectorAll(".faq__item");
+
+faqItems.forEach((item) => {
+  const faqBtn = item.querySelector(".faq__btn");
+
+  item.addEventListener("click", () => {
+    const isOpen = item.classList.toggle("open");
+    const iconClass = isOpen ? "ri-subtract-fill" : "ri-add-fill";
+    const iconElement = faqBtn.querySelector("i");
+    iconElement.classList = `${iconClass} text-2xl`;
+  });
+});
