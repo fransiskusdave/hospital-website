f6777b6c55d18313b2996acdbd938cd4f285d42a
Fransiskus Dave <davefransiskus@gmail.com>
2024-02-07 23:17:11 +0700

    added scroll reveal animations

--- dist/js/main.js
+++ dist/js/main.js
@@ -45,3 +45,77 @@ faqItems.forEach((item) => {
     iconElement.classList = `${iconClass} text-2xl`;
   });
 });
+
+// Scroll Reveal Animation
+const sr = ScrollReveal({
+  origin: "bottom",
+  distance: "60px",
+  duration: 3000,
+  delay: 600,
+  // reset: true // reset animations
+});
+
+sr.reveal(".hero__text", { origin: "top" });
+sr.reveal(".hero__img");
+
+sr.reveal(".statistics__item", {
+  origin: "top",
+  distance: "100px",
+  interval: 100,
+  delay: 600,
+});
+
+sr.reveal(".services");
+sr.reveal(".services__top");
+sr.reveal(".services__item", {
+  origin: "bottom",
+  distance: "100px",
+  interval: 100,
+  delay: 600,
+});
+
+sr.reveal(".appointment__title");
+sr.reveal(".appointment__form");
+
+sr.reveal(".testimonials");
+sr.reveal(".testimonials__container");
+
+sr.reveal(".team__title");
+sr.reveal(".team__slider");
+
+sr.reveal(".faq__title");
+sr.reveal(".faq__item", {
+  origin: "bottom",
+  distance: "100px",
+  interval: 100,
+  delay: 600,
+});
+
+sr.reveal(".departments__bg");
+sr.reveal(".departments__container");
+
+sr.reveal(".blog__title");
+sr.reveal(".blog__post", {
+  origin: "bottom",
+  delay: 600,
+  interval: 100,
+  distance: "100px",
+});
+
+sr.reveal(".brand__title");
+sr.reveal(".brand__logo", {
+  origin: "bottom",
+  delay: 600,
+  interval: 100,
+  distance: "100px",
+});
+
+sr.reveal(".newsletter");
+sr.reveal(".newsletter__container");
+
+sr.reveal(".footer__item", {
+  origin: "bottom",
+  delay: 600,
+  interval: 100,
+  distance: "100px",
+});
